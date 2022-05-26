5c9b8ae9f88c4e28112dbef4d51b74117b463750
TakuanNagoshika33S <106127153+TakuanNagoshika33S@users.noreply.github.com>
2022-05-26 09:01:57 +0900

    Update hello,world.java

--- hello,world.java
+++ hello,world.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         
         //Hello World‚ð•\Ž¦‚·‚é
-        System.out.printn("Hello World");
+        System.out.println("Hello World")
  
     }
 }
